0a05b9645dc063c48f9c9ac40d7887af4684c694
Ben Tonks <thebentonks@gmail.com>
2019-10-17 14:39:41 +0100

    Removed testing folder

--- testing/gravBallSpec.js
+++
@@ -1,9 +0,0 @@
-(document).onload(function(){
-    describe("gravBALL", function() {
-        decribe("Game mechanic tests", function(){
-            it("should turn gravity off", function(){
-                expect(canvas.addEventListener('mousedown')).to
-            })
-        })
-    })
-})
