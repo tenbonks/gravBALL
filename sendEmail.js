9976f71d7cb07ea1af145110edea57725074f04b
Ben Tonks <thebentonks@gmail.com>
2019-10-19 17:08:08 +0100

    initial commit of sendEmail.js

---
+++ sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail_gb", "ben", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
